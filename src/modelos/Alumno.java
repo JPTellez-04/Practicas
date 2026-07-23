8e093b1ed77fb700b1d61a237d9a911c538691f5
Juan Pablo Tellez Flores <tellez.juan.4427@gmail.com>
2026-07-23 12:14:13 -0600

    Finished the Class named Alumno.java

--- src/modelos/Alumno.java
+++ src/modelos/Alumno.java
@@ -1,6 +1,8 @@
 package modelos;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Alumno {
     /* Atributes */
@@ -34,7 +36,81 @@ public class Alumno {
                 Connection con = Conexion.createConnection();
                 PreparedStatement stmt = con.prepareStatement("INSERT INTO Alumno(matricula,nombre,edad,sexo,correo) VALUES (?,?,?,?,?)", Statement.RETURN_GENERATED_KEYS);
                 ){
+            stmt.setString(1, this.matricula);
+            stmt.setString(2, this.nombre);
+            stmt.setInt(3, this.edad);
+            stmt.setString(4, this.sexo);
+            stmt.setString(5, this.correo);
+            stmt.executeUpdate();
 
+            ResultSet rs = stmt.getGeneratedKeys();
+            if (rs.next()) {
+                this.id = rs.getInt(1);
+            }
+        }catch (Exception e){
+            throw new SQLException();
+        }
+    }
+
+    public static List<Alumno> findAll() throws SQLException {
+        try(
+                Connection con = Conexion.createConnection();
+                PreparedStatement stmt = con.prepareStatement("SELECT * FROM Alumno");
+                ){
+            ResultSet rs = stmt.executeQuery();
+            List<Alumno> alumnos = new ArrayList<>();
+            while(rs.next()){
+                alumnos.add(new Alumno(
+                        rs.getInt(1),
+                        rs.getString(2),
+                        rs.getString(3),
+                        rs.getInt(4),
+                        rs.getString(5),
+                        rs.getString(6)
+                ));
+            }
+            return  alumnos;
+        }catch (Exception e){
+            throw new SQLException();
+        }
+    }
+    public static void updateStudentByMatricula(String matricula, String nombre, Integer edad, String sexo, String correo) throws SQLException {
+        try(
+                Connection con = Conexion.createConnection();
+                PreparedStatement stmt = con.prepareStatement("UPDATE Alumno SET nombre=?, edad = ?, sexo=?, correo=? WHERE matricula=?");
+        ){
+            stmt.setString(1, nombre);
+            stmt.setInt(2, edad);
+            stmt.setString(3, sexo);
+            stmt.setString(4, correo);
+            stmt.executeUpdate();
+        }catch (Exception e){
+            throw new SQLException();
+        }
+    }
+    public static void deleteStudentByMatricula(String matricula) throws SQLException {
+        try(
+                Connection con = Conexion.createConnection();
+                PreparedStatement stmt = con.prepareStatement("DELETE FROM Alumno WHERE matricula=?");
+        ){
+            stmt.setString(1, matricula);
+            stmt.executeUpdate();
+        }catch (Exception e){
+            throw new SQLException();
+        }
+    }
+    public static Integer numberOfMen() throws SQLException {
+        try(
+                Connection con = Conexion.createConnection();
+                PreparedStatement stmt = con.prepareStatement("SELECT COUNT(*) FROM Alumno WHERE sexo = 'Hombre'");
+                ){
+            ResultSet rs = stmt.executeQuery();
+            if(rs.next()){
+                return rs.getInt(1);
+            }
+            return null;
+        }catch (Exception e){
+            throw new SQLException();
         }
     }
 
